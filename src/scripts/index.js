7e17600957180ec3c7d9308d7720c7f3452e4d36
Bartek <bartek.szajna@wp.pl>
2020-12-22 16:25:20 +0100

    Working on mobile view of the main and login pages

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,18 @@
 import '../styles/index.scss';
+
+const loginButton = document.querySelector('.login-button');
+const backButton = document.querySelector('.back-button');
+const modal = document.querySelector('.modal');
+const logo = document.querySelector('.logo');
+
+loginButton.addEventListener('click', (e) => {
+  modal.classList.add('modal-open');
+  logo.style.zIndex = 2;
+  modal.setAttribute('aria-label', 'modal open');
+});
+
+backButton.addEventListener('click', (e) => {
+  modal.classList.remove('modal-open');
+  logo.style.zIndex = 1;
+  modal.setAttribute('aria-label', 'modal closed');
+});
