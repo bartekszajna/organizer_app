940f5d0ec2189b860a3a9cc794673d24f497443d
Bartek <bartek.szajna@wp.pl>
2020-12-24 15:04:15 +0100

    Adding interactivity to login modal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,16 +3,73 @@ import '../styles/index.scss';
 const loginButton = document.querySelector('.login-button');
 const backButton = document.querySelector('.back-button');
 const modal = document.querySelector('.modal');
+const firstEl = modal.querySelector('#username');
+const lastEl = modal.querySelector('.back-button');
+const form = document.querySelector('.login-form');
 const logo = document.querySelector('.logo');
 
-loginButton.addEventListener('click', (e) => {
-  modal.classList.add('modal-open');
-  logo.style.zIndex = 2;
-  modal.setAttribute('aria-label', 'modal open');
-});
+loginButton.addEventListener('click', openModal);
 
-backButton.addEventListener('click', (e) => {
-  modal.classList.remove('modal-open');
+document.addEventListener('keydown', detectEscapeEvent);
+
+backButton.addEventListener('click', closeModal);
+
+modal.addEventListener('click', detectClickOutsideModal);
+
+let flag = false;
+
+function openModal(e) {
+  //modal.style.display = 'flex';
+  modal.classList.add('modal-open');
+  form.classList.add('login-form-open');
+
+  // modal.addEventListener('transitionend', () => {
+  //   if (flag) {
+  //     flag = true;
+  //     firstEl.focus();
+  //   }
+  // });
   logo.style.zIndex = 1;
-  modal.setAttribute('aria-label', 'modal closed');
-});
+  loginButton.setAttribute('aria-expanded', 'true');
+  firstEl.addEventListener('focus', handleFirstEl);
+  lastEl.addEventListener('focus', handleLastEl);
+}
+
+function closeModal(e) {
+  modal.classList.remove('modal-open');
+  logo.style.zIndex = 0;
+  loginButton.focus();
+  loginButton.setAttribute('aria-expanded', 'false');
+  firstEl.removeEventListener('focus', handleFirstEl);
+  lastEl.removeEventListener('focus', handleLastEl);
+}
+
+function detectEscapeEvent(e) {
+  if (e.code === 'Escape') {
+    backButton.click();
+  }
+}
+
+function detectClickOutsideModal(e) {
+  if (!form.contains(e.target)) {
+    backButton.click();
+  }
+}
+
+function handleFirstEl(e) {
+  e.target.addEventListener('keydown', (e) => {
+    if (e.shiftKey && e.code === 'Tab') {
+      e.preventDefault();
+      lastEl.focus();
+    }
+  });
+}
+
+function handleLastEl(e) {
+  e.target.addEventListener('keydown', (e) => {
+    if (e.code === 'Tab' && !e.shiftKey) {
+      e.preventDefault();
+      firstEl.focus();
+    }
+  });
+}
