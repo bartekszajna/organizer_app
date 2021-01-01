0ea60d78e69f40b1f591868649b4b86adc42b53b
Bartek <bartek.szajna@wp.pl>
2021-01-01 22:31:50 +0100

    Updated profile page
    Finished both mobile and desktop views. There is still some JS
    interactions left to be added.

--- src/scripts/profile.js
+++ src/scripts/profile.js
@@ -3,6 +3,9 @@ import '../styles/profile.scss';
 const hamburgerButton = document.querySelector('.hamburger');
 const navigation = document.querySelector('.list');
 const body = document.querySelector('body');
+const modal = document.querySelector('.modal');
+const backButton = document.querySelector('.back-button');
+const addTaskButton = document.querySelector('.addtask-button');
 
 hamburgerButton.addEventListener('click', (e) => {
   hamburgerButton.classList.toggle('hamburger-open');
@@ -17,4 +20,20 @@ document.fonts.ready.then(showBody);
 
 function showBody() {
   body.classList.add('visible');
+}
+
+addTaskButton.addEventListener('click', openModal);
+
+backButton.addEventListener('click', closeModal);
+
+function openModal() {
+  modal.classList.add('open');
+  modal.classList.add('modal-open');
+  addTaskButton.setAttribute('aria-expanded', 'true');
+}
+
+function closeModal() {
+  modal.classList.remove('modal-open');
+  addTaskButton.focus();
+  addTaskButton.setAttribute('aria-expanded', 'false');
 }
